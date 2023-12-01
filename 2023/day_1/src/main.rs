6d0cd3ff57fe47b00cc8058b85c6fec74fe7a084
William Boudreault <boudreaultwilliam@gmail.com>
2023-12-01 17:53:26 -0500

    day 1 part 2

--- 2023/day_1/src/main.rs
+++ 2023/day_1/src/main.rs
@@ -5,20 +5,31 @@ fn main() {
         let mut left_most = ' ';
         let mut right_most = ' ';
 
-        for char in line.chars() {
-            match char {
-                '0'..='9' => {
-                    if left_most == ' ' {
-                        left_most = char;
-                    }
-                    right_most = char;
-                },
-                _ => {}
-            }
+        for index in 0..line.len() {
+            let remaining_line = &line[index..];
+
+            let char = get_first_number(&remaining_line);
+
+            if let '0'..='9' = char {
+                if left_most == ' ' {
+                    left_most = char;
+                }
+                right_most = char;
+            } 
         }
 
         return format!("{}{}", left_most, right_most).parse::<u32>().unwrap();
     }).sum();
 
     println!("{num}");
+}
+
+fn get_first_number(input: &str) -> char {
+    if input.chars().next().unwrap().is_ascii_digit() {
+        return input.chars().next().unwrap();
+    }
+    
+    let numbers = [("one",'1'),  ("two",'2'), ("three", '3'), ("four", '4'), ("five", '5'), ("six", '6'), ("seven", '7'), ("eight",'8'),  ("nine",'9')];
+
+    return numbers.iter().find(|x| input.starts_with(x.0)).unwrap_or(&("", ' ')).1;
 }
