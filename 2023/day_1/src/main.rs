3dec8f5fa8d52da2a31aba46aa1a5352438a7de0
William Boudreault <boudreaultwilliam@gmail.com>
2023-12-01 17:31:51 -0500

    day 1 part 1

--- 2023/day_1/src/main.rs
+++ 2023/day_1/src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("../input.txt");
+
+    let num: u32 = input.lines().map(|line| {
+        let mut left_most = ' ';
+        let mut right_most = ' ';
+
+        for char in line.chars() {
+            match char {
+                '0'..='9' => {
+                    if left_most == ' ' {
+                        left_most = char;
+                    }
+                    right_most = char;
+                },
+                _ => {}
+            }
+        }
+
+        return format!("{}{}", left_most, right_most).parse::<u32>().unwrap();
+    }).sum();
+
+    println!("{num}");
 }
