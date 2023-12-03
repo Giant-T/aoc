26cd11d5df2f3203e0cf8348bcfec5709d6ba205
William Boudreault <boudreaultwilliam@gmail.com>
2023-12-03 18:08:37 -0500

    day 3 part 1

--- 2023/day_3/src/main.rs
+++ 2023/day_3/src/main.rs
@@ -1,3 +1,54 @@
+fn main() {
+    let input = include_str!("../input.txt");
+
+    let lines = input
+            .lines()
+            .collect::<Vec<_>>();
+
+    let sum: u32 = lines
+        .iter()
+        .enumerate()
+        .map(|(y, line)| {
+            let mut num = String::new();
+            let mut is_part = false;
+
+            return line
+                .char_indices()
+                .filter_map(|(x, ch)| {
+                    if ch.is_ascii_digit() {
+                        num.push(ch);
+
+                        if let Some(py) = y.checked_sub(1) {
+                            let prev_line = lines[py];
+                            is_part |= check_special_near(prev_line, x);
+                        }
+                        if let Some(next_line) = lines.get(y + 1) {
+                            is_part |= check_special_near(next_line, x);
+                        }
+                        is_part |= check_special_near(line, x);
+                    }
+
+                    if x == (line.len() -1) || !ch.is_ascii_digit() {
+                        let result = num.parse::<u32>().ok(); 
+                        num.clear();
+
+                        if is_part {
+                            is_part = false;
+                            return result;
+                        }
+
+                        is_part = false;
+                    }
+
+                    return None;
+                })
+                .sum::<u32>();
+        })
+        .sum();
+
+    println!("{:?}", sum);
+}
+
 fn is_special_char(ch: &u8) -> bool {
     *ch != b'.' && !ch.is_ascii_digit()
 }
@@ -19,56 +70,3 @@ fn check_special_near(line: &str, x: usize) -> bool {
     return is_part;
 }
 
-fn main() {
-    let input = include_str!("../input.txt");
-
-    let lines = input
-            .lines()
-            .collect::<Vec<_>>();
-
-    let sum: u32 = lines
-        .iter()
-        .enumerate()
-        .map(|(y, line)| {
-            let mut num = String::new();
-            let mut is_part = false;
-
-            return line
-                .char_indices()
-                .filter_map(|(x, ch)| {
-                    if ch.is_ascii_digit() {
-                        num.push(ch);
-
-                        if is_part { // Return early if is already a part
-                            return None;
-                        }
-
-                        if let Some(py) = y.checked_sub(1) {
-                            let prev_line = lines[py];
-                            is_part |= check_special_near(prev_line, x);
-                        }
-                        if let Some(next_line) = lines.get(y + 1) {
-                            is_part |= check_special_near(next_line, x);
-                        }
-                        is_part |= check_special_near(line, x);
-
-                    } else {
-                        let result = num.parse::<u32>().ok(); 
-                        num.clear();
-
-                        if is_part {
-                            is_part = false;
-                            return result;
-                        }
-
-                        is_part = false;
-                    }
-
-                    return None;
-                })
-                .sum::<u32>();
-        })
-        .sum();
-
-    println!("{:?}", sum);
-}
