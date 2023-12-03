072410b3f2f22aa541ef02a359413b431c17c0d3
William Boudreault <boudreaultwilliam@gmail.com>
2023-12-03 18:34:30 -0500

    day 3 part 2

--- 2023/day_3/src/main.rs
+++ 2023/day_3/src/main.rs
@@ -1,58 +1,59 @@
+use std::collections::HashMap;
+
 fn main() {
     let input = include_str!("../input.txt");
 
-    let lines = input
-            .lines()
-            .collect::<Vec<_>>();
+    let lines = input.lines().collect::<Vec<_>>();
 
-    let sum: u32 = lines
-        .iter()
-        .enumerate()
-        .map(|(y, line)| {
-            let mut num = String::new();
-            let mut is_part = false;
+    let mut gear_map = HashMap::<(usize, usize), Vec<u32>>::new();
 
-            return line
-                .char_indices()
-                .filter_map(|(x, ch)| {
-                    if ch.is_ascii_digit() {
-                        num.push(ch);
+    lines.iter().enumerate().for_each(|(y, line)| {
+        let mut num = String::new();
+        let mut gear: Option<(usize, usize)> = None;
 
-                        if let Some(py) = y.checked_sub(1) {
-                            let prev_line = lines[py];
-                            is_part |= check_special_near(prev_line, x);
-                        }
-                        if let Some(next_line) = lines.get(y + 1) {
-                            is_part |= check_special_near(next_line, x);
-                        }
-                        is_part |= check_special_near(line, x);
+        line.char_indices().for_each(|(x, ch)| {
+            if ch.is_ascii_digit() {
+                num.push(ch);
+
+                if let None = gear {
+                    gear = find_nearby_gear(&lines, (x, y));
+                }
+            }
+
+            if x == (line.len() - 1) || !ch.is_ascii_digit() {
+                let result = num.parse::<u32>().ok();
+                num.clear();
+
+                if let Some(coords) = gear {
+                    if let Some(vec) = gear_map.get_mut(&coords) {
+                        vec.push(result.unwrap());
+                    } else {
+                        gear_map.insert(coords, vec![result.unwrap()]);
                     }
+                }
 
-                    if x == (line.len() -1) || !ch.is_ascii_digit() {
-                        let result = num.parse::<u32>().ok(); 
-                        num.clear();
+                gear = None;
+            }
+        });
+    });
 
-                        if is_part {
-                            is_part = false;
-                            return result;
-                        }
+    let sum: u32 = gear_map.iter().filter_map(|(_, v)| {
+        if v.len() == 2 {
+            return Some(v[0] * v[1]);
+        }
 
-                        is_part = false;
-                    }
+        return None;
+    }).sum();
 
-                    return None;
-                })
-                .sum::<u32>();
-        })
-        .sum();
-
-    println!("{:?}", sum);
+    println!("{sum}");
 }
 
+#[allow(dead_code)]
 fn is_special_char(ch: &u8) -> bool {
     *ch != b'.' && !ch.is_ascii_digit()
 }
 
+#[allow(dead_code)]
 fn check_special_near(line: &str, x: usize) -> bool {
     let mut is_part = false;
     let line = line.as_bytes();
@@ -70,3 +71,44 @@ fn check_special_near(line: &str, x: usize) -> bool {
     return is_part;
 }
 
+fn find_nearby_gear(lines: &Vec<&str>, coords: (usize, usize)) -> Option<(usize, usize)> {
+    if let Some(py) = coords.1.checked_sub(1) {
+        if let Some(x) = find_gear_on_line(lines[py], coords.0) {
+            return Some((x, py));
+        }
+    }
+
+    if let Some(x) = find_gear_on_line(lines[coords.1], coords.0) {
+        return Some((x, coords.1));
+    }
+
+    if let Some(n_line) = lines.get(coords.1 + 1) {
+        if let Some(x) = find_gear_on_line(n_line, coords.0) {
+            return Some((x, coords.1 + 1));
+        }
+    }
+
+    return None;
+}
+
+fn find_gear_on_line(line: &str, x: usize) -> Option<usize> {
+    let line = line.as_bytes();
+
+    if let Some(px) = x.checked_sub(1) {
+        if line[px] == b'*' {
+            return Some(px);
+        }
+    }
+
+    if line[x] == b'*' {
+        return Some(x);
+    }
+
+    if let Some(ch) = line.get(x + 1) {
+        if *ch == b'*' {
+            return Some(x + 1);
+        }
+    }
+
+    return None;
+}
