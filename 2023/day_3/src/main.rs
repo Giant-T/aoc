66f19666ac6a55a7b81060d9670ba9316b9e5a40
William Boudreault <boudreaultwilliam@gmail.com>
2023-12-03 17:41:50 -0500

    day 3 part 1 wip

--- 2023/day_3/src/main.rs
+++ 2023/day_3/src/main.rs
@@ -1,86 +1,74 @@
-struct Part {
-    num: String,
-    pub is_part: bool,
-}
-
-impl Part {
-    pub fn new() -> Self {
-        Self {
-            num: String::new(),
-            is_part: false,
-        }
-    }
-
-    pub fn push(&mut self, ch: char) {
-        self.num.push(ch);
-    }
-
-    pub fn clear(&mut self) {
-        self.num.clear();
-        self.is_part = false;
-    }
-
-    pub fn get_num(&mut self) -> u32 {
-        self.num.parse().unwrap()
-    }
-}
-
 fn is_special_char(ch: &u8) -> bool {
     *ch != b'.' && !ch.is_ascii_digit()
 }
 
-fn main() {
-    let input = include_str!("../sample.txt");
+fn check_special_near(line: &str, x: usize) -> bool {
+    let mut is_part = false;
+    let line = line.as_bytes();
 
-    let lines = input
-        .lines()
-        .collect::<Vec<_>>();
-
-    let mut sum = 0;
-
-    for (y, line) in lines.iter().enumerate() {
-        let mut part = Part::new();
-        for (x, char) in line.chars().enumerate() {
-            if char.is_ascii_digit() {
-                part.push(char);
-
-                if let Some(minus_y) = y.checked_sub(1) {
-                    let previous_line = lines[minus_y].as_bytes();
-                    for i in 0..3 {
-                        if let Some(index) = (x + i).checked_sub(1) {
-                            if let Some(char) = previous_line.get(index) {
-                                part.is_part |= is_special_char(char);
-                            }
-                        }
-                    }
-                }
-
-                if let Some(index) = x.checked_sub(1) {
-                    let prev_char = line.as_bytes()[index];
-                    part.is_part |= is_special_char(&prev_char);
-                }
-                if let Some(next_char) = line.as_bytes().get(x + 1) {
-                    part.is_part |= is_special_char(next_char);
-                }
-
-                if let Some(next_line) = lines.get(y + 1) {
-                    for i in 0..3 {
-                        let next_line = next_line.as_bytes();
-                        if let Some(index) = (x + i).checked_sub(1) {
-                            if let Some(char) = next_line.get(index) {
-                                part.is_part |= is_special_char(char);
-                            }
-                        }
-                    }
-                }
-            } else {
-                if part.is_part {
-                    sum += part.get_num();
-                }
-                part.clear();
-            }
-        }
+    if let Some(px) = x.checked_sub(1) {
+        is_part |= is_special_char(&line[px]);
     }
 
-    println!("{sum}");
+    is_part |= is_special_char(&line[x]);
+
+    if let Some(ch) = line.get(x + 1) {
+        is_part |= is_special_char(ch);
+    }
+
+    return is_part;
+}
+
+fn main() {
+    let input = include_str!("../input.txt");
+
+    let lines = input
+            .lines()
+            .collect::<Vec<_>>();
+
+    let sum: u32 = lines
+        .iter()
+        .enumerate()
+        .map(|(y, line)| {
+            let mut num = String::new();
+            let mut is_part = false;
+
+            return line
+                .char_indices()
+                .filter_map(|(x, ch)| {
+                    if ch.is_ascii_digit() {
+                        num.push(ch);
+
+                        if is_part { // Return early if is already a part
+                            return None;
+                        }
+
+                        if let Some(py) = y.checked_sub(1) {
+                            let prev_line = lines[py];
+                            is_part |= check_special_near(prev_line, x);
+                        }
+                        if let Some(next_line) = lines.get(y + 1) {
+                            is_part |= check_special_near(next_line, x);
+                        }
+                        is_part |= check_special_near(line, x);
+
+                    } else {
+                        let result = num.parse::<u32>().ok(); 
+                        num.clear();
+
+                        if is_part {
+                            is_part = false;
+                            return result;
+                        }
+
+                        is_part = false;
+                    }
+
+                    return None;
+                })
+                .sum::<u32>();
+        })
+        .sum();
+
+    println!("{:?}", sum);
 }
